a3a65e3a26ebc06a646a85ec5fff99e278e95c2f
1435896043@qq.com <1435896043@qq.com>
2019-07-15 11:57:41 +0800

    jquery引入提交

--- src/components/popGroup/popEleContent/latlngLocation.vue
+++ src/components/popGroup/popEleContent/latlngLocation.vue
@@ -2,12 +2,12 @@
   <div class="latlng_location_content">
     <div class="latlng_location_ele_box">
       <span class="latlng_location_text">经度:</span>
-      <el-input placeholder="请输入经度" v-model="inputLng"  class="latlng_location_input" oninput = "value=value.replace(/[^\d.]/g,'')"></el-input>
+      <el-input placeholder="请输入经度,范围<73.66666~135.0416666>" v-model="inputLng"  class="latlng_location_input" oninput = "value=value.replace(/[^\d.]/g,'')"></el-input>
     </div>
 
     <div class="latlng_location_ele_box">
      <span class="latlng_location_text">纬度:</span>
-      <el-input placeholder="请输入纬度" v-model="inputLat" class="latlng_location_input" oninput = "value=value.replace(/[^\d.]/g,'')"></el-input>
+      <el-input placeholder="请输入纬度,范围<3.8666666~53.55>" v-model="inputLat" class="latlng_location_input" oninput = "value=value.replace(/[^\d.]/g,'')"></el-input>
     </div>
   </div>
 </template>
@@ -25,9 +25,15 @@
     },
     methods:{
       rightPopCallBackFun:function(){
-        debugger
         let lng=this.inputLng;
         let lat=this.inputLat;
+        if(lng*1>135.0416666||lng*1<73.66666){
+          alert("请输入国界范围内经度");
+          return;
+        }else if(lat*1>53.55||lat*1<3.8666666){
+          alert("请输入国界范围内纬度")
+          return;
+        }
         let mapNew = this.$store.getters["mainStore/getMapFn"];
         mapNew.panTo(new L.LatLng(lat, lng), {
           animate: true,
