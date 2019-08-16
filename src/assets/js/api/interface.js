cdd5f59323befd825bd4c2695673734b4d666928
1435896043@qq.com <1435896043@qq.com>
2019-08-16 18:52:11 +0800

    路由代码提交

--- src/assets/js/api/interface.js
+++ src/assets/js/api/interface.js
@@ -1,3 +1,13 @@
+import axios from 'axios'
+
 export function test() {
-  debugger
+  // return axios.get(url81 + `/plan/one?id=${id}`)//get请求
+  // return axios.post(url81 + '/plan/upd',param)//post请求
+
+  // let formData = new FormData();
+  // formData.append("filename", "my-file");  // 文件名
+  // formData.append("operation","upload_file")
+  // formData.append("file", file);//file是blob文件
+  // formData.append("angle1","0");
+  // return axios.post(url81 + '/tmc/upd', param, {headers: {"Content-Type": "multipart/form-data"}});//fromdate
 }
